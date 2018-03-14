0e464e95ce7005e97d5b956b32f8ebebf4b2d430
Satish Bhambri <sbhambri@asu.edu>
2018-03-13 22:50:00 -0700

    Cards Shuffle

---
+++ Desktop/Puzzles and Leetcode/CardsShuffle.py
@@ -0,0 +1,16 @@
+# Python program to shuffle a deck of card using the module random and draw 5 cards
+
+# import modules
+import itertools, random
+
+# make a deck of cards
+deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+print(deck)
+
+# shuffle the cards
+random.shuffle(deck)
+
+# draw five cards
+print("You got:")
+for i in range(5):
+   print(deck[i][0], "of", deck[i][1])
